b3b65d89732ea5245b16906a0b61f2e3c6cbfac9
Kirtan <kirtan.rajesh2021@vitstudent.ac.in>
2023-06-04 17:12:25 +0530

    learned about api

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,24 @@
+const express= require("express");
+const bodyParser = require("body-parser");
+const request =require("request");
+
+
+const app =express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    var password=req.body.pswd;
+    var email=req.body.email;
+
+    console.log(email,password);
+})
+
+app.listen(3000,function(){
+    console.log("server is running on 3000");
+}); 
--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,10 +1,25 @@
 const express= require("express");
 const app=express();
 const https=require("https");
-
 app.get("/",function(req,res){
-    https.get()
-    res.send("Server is up and running.")
+    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=71ca5b3e5f132bc7996ac79a521296d4";
+
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data)
+            const temp=weatherData.main.temp
+            const weatherDescription= weatherData.weather[0].description
+            const icon=weatherData.weather[0].icon
+            const imageURL="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            res.write("<p>The weather is currently "+weatherDescription+"<p>");
+            res.write("<h1>The temperature in London is "+temp+" degrees Celcius.</h1>");
+            res.write("<img src=" +imageURL+">");
+            res.send();
+        })
+
+    })
 })
 
 app.listen(3000,function(){
